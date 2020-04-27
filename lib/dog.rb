47b78db9b67998f303dc329ea7f47f91725eaa22
Daniel Amyakar <dan.rockit@gmail.com>
2020-04-27 19:38:42 +0000

    Automatically backed up by Learn

--- lib/dog.rb
+++ lib/dog.rb
@@ -3,6 +3,17 @@ class Dog
     @name = dog_name
     @breed = dog_breed
   end
-  
+  def name=(dog_name)
+    @name = dog_name
+  end
+  def name
+    @name
+  end
+  def breed=(dog_breed)
+    @breed = dog_breed
+  end
+  def breed
+    @breed
+  end
     
 end
